0356d5346543ab161e589b025496281ee03e5f80
Javiani <eduardo.ottaviani@gmail.com>
2024-04-08 20:02:11 -0300

    Finishing main

--- src/shared/main.ts
+++ src/shared/main.ts
@@ -1,15 +1,4 @@
 
 import jails from 'jails-js'
 
-export const dependencies = {}
-
-window.MFE = {
-
-    register(name, module, dependencies) {
-        jails.register(name, module, dependencies )
-    },
-
-    start() {
-        jails.start()
-    }
-}
+window.MFE = jails 
