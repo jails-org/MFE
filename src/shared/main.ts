16c2e2a7b0a2c158bedc104652eefb9a7c97facc
Javiani <eduardo.ottaviani@gmail.com>
2024-04-08 21:27:15 -0300

    Simpflifying things

--- src/shared/main.ts
+++ src/shared/main.ts
@@ -1,47 +1,42 @@
 
 import jails from 'jails-js'
 
+const origin = 'https://mfe-jails.netlify.app'
+const mfes = []
+
 window.MFE = {
 
     register( name, module, dependencies ) {
         jails.register( name, module, dependencies )
     },
 
-    install( origin ) {
-
-        const mfes = []
+    add( name, target ) {
+    
+        const link = document.createElement('link')   
+        link.rel = 'stylesheet'
+        link.href = `${origin}/${name}/index.css`
         
-        return {
-            
-            add( name, target ) {
-    
-                const link = document.createElement('link')   
-                link.rel = 'stylesheet'
-                link.href = `${origin}/${name}/index.css`
-                
-                document.head.appendChild( link )
-    
-                fetch(`${origin}/${name}/index.html`)
-                    .then( res => res.text() )
-                    .then( h => document.querySelector( target ).innerHTML = h )
-    
-                const app = document.createElement('script')
-                app.defer = true 
-                
-                mfes.push(
-                    new Promise((resolve) => {
-                        app.src = `${origin}/${name}/index.js`
-                        app.onload = resolve
-                        document.head.appendChild( app )
-                    })
-                )
-    
-                return this
-            },
-    
-            start() {
-                Promise.allSettled(mfes).then( _ => jails.start() )
-            }
-        }    
+        document.head.appendChild( link )
+
+        fetch(`${origin}/${name}/index.html`)
+            .then( res => res.text() )
+            .then( h => document.querySelector( target ).innerHTML = h )
+
+        const app = document.createElement('script')
+        app.defer = true 
+        
+        mfes.push(
+            new Promise((resolve) => {
+                app.src = `${origin}/${name}/index.js`
+                app.onload = resolve
+                document.head.appendChild( app )
+            })
+        )
+
+        return this
+    },
+
+    start() {
+        Promise.allSettled(mfes).then( _ => jails.start() )
     }
 }
