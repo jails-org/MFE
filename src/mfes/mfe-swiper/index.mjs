84699451dc21bf819d9d489fdc3a3934c7adb362
Javiani <eduardo.ottaviani@gmail.com>
2025-04-18 20:06:50 -0300

    Improving skeleton

---
+++ src/mfes/mfe-swiper/index.mjs
@@ -0,0 +1,1 @@
+export * as appSwiper from './app'
--- src/mfes/mfe-swiper/index.ts
+++
@@ -1,9 +0,0 @@
-
-import * as appSwiper from './app'
-
-window.webcomponent = window.webcomponent || {}
-window.webcomponent.appSwiper = appSwiper
-
-if( window.MFE ) {
-	window.MFE.register('app-swiper', appSwiper, { Swiper: window.Swiper })
-}
