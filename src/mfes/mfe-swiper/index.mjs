e6144c292ad9140d38cb39b4e5e71d4d42340f49
Javiani <eduardo.ottaviani@gmail.com>
2025-04-18 21:49:46 -0300

    Adding swiper to the global scope

--- src/mfes/mfe-swiper/index.mjs
+++
@@ -1,3 +0,0 @@
-import * as appSwiper from './app'
-
-export default appSwiper
---
+++ src/mfes/mfe-swiper/index.ts
@@ -0,0 +1,4 @@
+import * as appSwiper from './app'
+
+window.webcomponents = window.webcomponents || {}
+window.webcomponents.appSwiper = appSwiper
